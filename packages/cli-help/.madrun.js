38fe6c69fd49f097bf3118b73a3536f912088cd4
coderiaser <coderaiser@cloudcmd.io>
2026-02-11 14:04:46 +0200

    chore: nirguna: versions

---
+++ packages/cli-help/.madrun.js
@@ -0,0 +1,10 @@
+export default {
+    'test': () => 'madrun test',
+    'watch:test': () => 'madrun watch:test',
+    'lint': () => 'putout .',
+    'fresh:lint': () => 'madrun fresh:lint',
+    'lint:fresh': () => 'madrun lint:fresh',
+    'fix:lint': () => 'madrun fix:lint',
+    'coverage': () => 'madrun coverage',
+    'report': () => 'madrun report',
+};
