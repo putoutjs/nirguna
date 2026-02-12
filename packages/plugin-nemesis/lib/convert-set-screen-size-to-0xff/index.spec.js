b3346ba97567ab0d8e99b6a6b94868fd36d34b00
coderiaser <coderaiser@cloudcmd.io>
2026-02-12 19:41:29 +0200

    test: @nirguna/plugin-nemesis: creen -> screen

--- packages/plugin-nemesis/lib/convert-set-screen-size-to-0xff/index.spec.js
+++ packages/plugin-nemesis/lib/convert-set-screen-size-to-0xff/index.spec.js
@@ -3,16 +3,16 @@ import * as plugin from './index.js';
 
 const test = createTest(import.meta.url, {
     plugins: [
-        ['convert-set-creen-size-to-0xff', plugin],
+        ['convert-set-screen-size-to-0xff', plugin],
     ],
 });
 
-test('nemesis: convert-set-creen-size-to-0xff: report', (t) => {
-    t.report('convert-set-creen-size-to-0xff', `Use '0xff' instead of 'nemesis.setScreenSize()'`);
+test('nemesis: convert-set-screen-size-to-0xff: report', (t) => {
+    t.report('convert-set-screen-size-to-0xff', `Use '0xff' instead of 'nemesis.setScreenSize()'`);
     t.end();
 });
 
-test('nemesis: convert-set-creen-size-to-0xff: transform', (t) => {
-    t.transform('convert-set-creen-size-to-0xff');
+test('nemesis: convert-set-screen-size-to-0xff: transform', (t) => {
+    t.transform('convert-set-screen-size-to-0xff');
     t.end();
 });
