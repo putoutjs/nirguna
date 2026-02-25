72014ea4de84b83c50d045e71b31cf66cdc5d957
coderiaser <coderaiser@cloudcmd.io>
2026-02-25 21:35:38 +0200

    test: @nirguna/transformer-wasm: apply private import

--- packages/transformer-wasm/test/transformer.js
+++ packages/transformer-wasm/test/transformer.js
@@ -1,6 +1,6 @@
 import {test} from 'supertape';
 import montag from 'montag';
-import {transform} from '../lib/transformer.js';
+import {transform} from '#transformer-wasm';
 
 test('nirguna: transformer-wasm', (t) => {
     const source = montag`
