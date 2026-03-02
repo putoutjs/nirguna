8680c9b8a132302d5e116d84e476ffaa3849d36d
coderiaser <coderaiser@cloudcmd.io>
2026-03-02 23:37:49 +0200

    feature: @nirguna/printer-fasm: rm unused

--- packages/printer-fasm/lib/printer/visitors/string-literal.js
+++ packages/printer-fasm/lib/printer/visitors/string-literal.js
@@ -1,13 +1,13 @@
-export const StringLiteral = (path, {write}, semantics) => {
+export const StringLiteral = (path, {write}) => {
     const {value, raw = `'${value}'`} = path.node;
     const newValue = raw.slice(1, -1);
     
     write.quote();
-    write(maybeEscape(newValue, semantics));
+    write(maybeEscape(newValue));
     write.quote();
 };
 
-const maybeEscape = (value, {escapeDoubleQuote, escapeSingleQuote}) => {
+const maybeEscape = (value) => {
     const list = value.split('');
     const slash = '\\';
     
@@ -21,7 +21,6 @@ const escape = (list, {slash, quote}) => {
     const result = [];
     
     for (const [index, char] of list.entries()) {
-        const prev = list[index - 1];
         const next = list[index + 1];
         
         if (char === slash && next === quote) {
