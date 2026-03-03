4194796855fff52f41172d3817446835473ae87c
coderiaser <coderaiser@cloudcmd.io>
2026-03-03 14:07:34 +0200

    chore: madrun: shorten: nirguna

--- packages/nemesis/.madrun.js
+++ packages/nemesis/.madrun.js
@@ -13,9 +13,9 @@ export default {
     'report': () => 'c8 report --reporter=lcov',
     'build': () => {
         return [
-            '../nirguna/bin/nirguna.js -t kernel lib/kernel.ts',
-            '../nirguna/bin/nirguna.js -t boot lib/boot/index.js',
-            '../nirguna/bin/nirguna.js -t nemesis lib/shell/shell.ts',
+            'nirguna -t kernel lib/kernel.ts',
+            'nirguna -t boot lib/boot/index.js',
+            'nirguna -t nemesis lib/shell/shell.ts',
             './scripts/build.js',
             'cp ./build/* ~/nemesis-emulator/',
         ].join(' && ');
