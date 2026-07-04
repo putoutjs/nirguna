927ab18c0f08e170f6d6684f22ae52281c171e5c
coderaiser <coderaiser@cloudcmd.io>
2026-07-04 18:32:58 +0000

    refactor: @nirguna/parser: parse: simplify

--- packages/parser-wasm/lib/parse/parse.js
+++ packages/parser-wasm/lib/parse/parse.js
@@ -8,45 +8,64 @@ const {program} = types;
 export const parse = (source) => {
     const ast = wastParse(source);
     const body = [];
-    
+
     traverse(ast, {
         Module(path) {
             const {fields} = path.node;
-            
-            // First pass: collect export names per func index
-            let funcIndex = -1;
-            const exports = {};
-            
-            for (const field of fields) {
-                if (field.type === 'Func')
-                    ++funcIndex;
-                
-                if (field.type === 'ModuleExport')
-                    exports[funcIndex] = field.name;
-            }
-            
-            // Second pass: process fields
-            funcIndex = -1;
-            
-            for (const field of fields) {
-                if (field.type === 'Func') {
-                    ++funcIndex;
-                    body.push(visitors.Func(field, exports[funcIndex]));
-                    continue;
-                }
-                
-                const fn = visitors[field.type];
-                
-                if (!fn)
-                    continue;
-                
-                const result = fn(field);
-                
-                if (result)
-                    body.push(result);
-            }
+
+            const exports = collectExports(fields);
+
+            body.push(
+                ...transformFields(fields, exports)
+            );
         },
     });
-    
+
     return program(body);
 };
+
+function collectExports(fields) {
+    const exports = {};
+    let funcIndex = -1;
+
+    for (const field of fields) {
+        if (field.type === 'Func') {
+            funcIndex++;
+            continue;
+        }
+
+        if (field.type === 'ModuleExport') {
+            exports[funcIndex] = field.name;
+        }
+    }
+
+    return exports;
+}
+
+function transformFields(fields, exports) {
+    const result = [];
+    let funcIndex = -1;
+
+    for (const field of fields) {
+        if (field.type === 'Func') {
+            funcIndex++;
+            result.push(
+                visitors.Func(field, exports[funcIndex])
+            );
+
+            continue;
+        }
+
+        const visit = visitors[field.type];
+
+        if (!visit)
+            continue;
+
+        const node = visit(field);
+
+        if (node)
+            result.push(node);
+    }
+
+    return result;
+}
