2540255ded90b72e0a68aeef4e17031d33720725
coderaiser <coderaiser@users.noreply.github.com>
2026-07-04 18:34:00 +0000

    chore: nirguna: actions: lint ☘️

--- packages/parser-wasm/lib/parse/parse.js
+++ packages/parser-wasm/lib/parse/parse.js
@@ -8,64 +8,59 @@ const {program} = types;
 export const parse = (source) => {
     const ast = wastParse(source);
     const body = [];
-
+    
     traverse(ast, {
         Module(path) {
             const {fields} = path.node;
-
+            
             const exports = collectExports(fields);
-
-            body.push(
-                ...transformFields(fields, exports)
-            );
+            
+            body.push(...transformFields(fields, exports));
         },
     });
-
+    
     return program(body);
 };
 
 function collectExports(fields) {
     const exports = {};
     let funcIndex = -1;
-
+    
     for (const field of fields) {
         if (field.type === 'Func') {
             funcIndex++;
             continue;
         }
-
-        if (field.type === 'ModuleExport') {
+        
+        if (field.type === 'ModuleExport')
             exports[funcIndex] = field.name;
-        }
     }
-
+    
     return exports;
 }
 
 function transformFields(fields, exports) {
     const result = [];
     let funcIndex = -1;
-
+    
     for (const field of fields) {
         if (field.type === 'Func') {
             funcIndex++;
-            result.push(
-                visitors.Func(field, exports[funcIndex])
-            );
-
+            result.push(visitors.Func(field, exports[funcIndex]));
+            
             continue;
         }
-
+        
         const visit = visitors[field.type];
-
+        
         if (!visit)
             continue;
-
+        
         const node = visit(field);
-
+        
         if (node)
             result.push(node);
     }
-
+    
     return result;
 }
