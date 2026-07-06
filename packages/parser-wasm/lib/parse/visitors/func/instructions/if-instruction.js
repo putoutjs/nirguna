40a4531f78b80d2adfee62275550184068c2d137
coderiaser <coderaiser@cloudcmd.io>
2026-07-07 01:40:00 +0300

    refactor: @nirgun/parser-wasm: func: ifInstruction: simplify

--- packages/parser-wasm/lib/parse/visitors/func/instructions/if-instruction.js
+++ packages/parser-wasm/lib/parse/visitors/func/instructions/if-instruction.js
@@ -3,9 +3,48 @@ import {types} from '@putout/babel';
 const {blockStatement, ifStatement} = types;
 
 export const IfInstruction = (instr, {labelKinds, emitExpression, emitStatement}) => {
-    const test = emitExpression(instr.test[0]);
-    const consequent = blockStatement(instr.consequent.map((i) => emitStatement(i, labelKinds)));
-    const alternate = instr.alternate.length ? blockStatement(instr.alternate.map((i) => emitStatement(i, labelKinds))) : null;
+    const test = createTest(instr, {
+        emitExpression,
+    });
+    
+    const consequent = createConsequent(instr, {
+        emitStatement,
+        labelKinds,
+    });
+    const alternate = createAlternate(instr, {
+        emitStatement,
+        labelKinds,
+    });
     
     return ifStatement(test, consequent, alternate);
 };
+
+function createTest(instr, {emitExpression}) {
+    const {test} = instr;
+    const [first] = test;
+    
+    return emitExpression(first);
+}
+
+function createConsequent(instr, {emitStatement, labelKinds}) {
+    const body = [];
+    
+    for (const consequent of instr.consequent) {
+        body.push(emitStatement(consequent, labelKinds));
+    }
+    
+    return blockStatement(body);
+}
+
+function createAlternate(instr, {emitStatement, labelKinds}) {
+    if (!instr.alternate.length)
+        return null;
+    
+    const body = [];
+    
+    for (const alternate of instr.alternate) {
+        body.push(emitStatement(alternate, labelKinds));
+    }
+    
+    return blockStatement(body);
+}
