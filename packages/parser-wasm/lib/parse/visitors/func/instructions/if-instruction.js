cc76f6b56a543309028cb2670c299975bc5c69fd
coderiaser <coderaiser@cloudcmd.io>
2026-07-07 01:33:45 +0300

    refactor: @nirguna/parser-wasm: func: instructions: if-instruction: simpmlify

--- packages/parser-wasm/lib/parse/visitors/func/instructions/if-instruction.js
+++ packages/parser-wasm/lib/parse/visitors/func/instructions/if-instruction.js
@@ -1,12 +1,12 @@
 import {types} from '@putout/babel';
 
-const {blockStatement} = types;
+const {blockStatement, ifStatement} = types;
 
 export const IfInstruction = (instr, {labelKinds, emitExpr, emitStatement}) => {
-    return {
-        type: 'IfStatement',
-        test: emitExpr(instr.test[0]),
-        consequent: blockStatement(instr.consequent.map((i) => emitStatement(i, labelKinds))),
-        alternate: instr.alternate.length ? blockStatement(instr.alternate.map((i) => emitStatement(i, labelKinds))) : null,
-    };
+    const test = emitExpr(instr.test[0]);
+    const consequent = blockStatement(instr.consequent.map((i) => emitStatement(i, labelKinds)));
+    const alternate = instr.alternate.length ? blockStatement(instr.alternate.map((i) => emitStatement(i, labelKinds))) : null;
+    
+    return ifStatement(test, consequent, alternate);
 };
+
