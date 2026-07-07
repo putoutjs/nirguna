88c02cbc29f031eba2e5e9cc54c2ea825d1a6286
coderiaser <coderaiser@cloudcmd.io>
2026-07-07 18:28:19 +0300

    refactor: @nirguna/parser-wasm: block-instruction: simplify

--- packages/parser-wasm/lib/parse/visitors/func/instructions/block-instruction.js
+++ packages/parser-wasm/lib/parse/visitors/func/instructions/block-instruction.js
@@ -4,12 +4,30 @@ const {
     blockStatement,
     labeledStatement,
     identifier,
+    BlockStatement,
 } = types;
 
 export const BlockInstruction = (instr, {labelKinds, emitStatement}) => {
     labelKinds.set(instr.label.value, 'block');
     
-    const body = blockStatement(instr.instr.map((i) => emitStatement(i, labelKinds)));
+    const body = getBody(instr, {
+        emitStatement,
+        labelKinds,
+    });
     
     return labeledStatement(identifier(instr.label.value), body);
 };
+
+function getBody(instr, {emitStatement, labelKinds}) {
+    const result = [];
+    
+    for (const instruction of instr.instr) {
+        result.push(emitStatement(
+            instruction,
+            labelKinds,
+        ));
+    }
+    
+    return blockStatement(result);
+}
+
