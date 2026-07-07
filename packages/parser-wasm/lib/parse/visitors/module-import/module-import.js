25a36c25a883a255d1dadb857da1f49d893b26f8
coderiaser <coderaiser@cloudcmd.io>
2026-07-07 18:57:22 +0300

    refactor: @nirguna/parser-wasm: module-import

--- packages/parser-wasm/lib/parse/visitors/module-import/module-import.js
+++ packages/parser-wasm/lib/parse/visitors/module-import/module-import.js
@@ -13,24 +13,26 @@ const {
 export const ModuleImport = ({module, name, descr}) => {
     const {id, signature} = descr;
     const fnName = id.value;
-    
-    const fnParams = signature.params.map(({id: paramId, valtype}) => {
-        const param = identifier(paramId || valtype);
-        
-        if (paramId)
-            param.typeAnnotation = typeAnnotation(valtype);
-        
-        return param;
-    });
+    const fnParams = signature.params.map(createParam);
+    const [firstResult] = signature.results;
     
     const fn = functionExpression(identifier(fnName), fnParams, blockStatement([]));
     
-    if (signature.results[0])
-        fn.returnType = typeAnnotation(signature.results[0]);
+    if (firstResult)
+        fn.returnType = typeAnnotation(firstResult);
     
     return expressionStatement(callExpression(identifier('__nirguna_wasm_import'), [
         stringLiteral(module),
         stringLiteral(name),
         fn,
     ]));
+};
+
+const createParam = ({id: paramId, valtype}) => {
+    const param = identifier(paramId || valtype);
+    
+    if (paramId)
+        param.typeAnnotation = typeAnnotation(valtype);
+    
+    return param;
 };
