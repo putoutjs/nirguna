30de2538c56346bc83bdaa66a2c0e2745df7e63e
coderiaser <coderaiser@cloudcmd.io>
2026-07-08 15:45:05 +0300

    chore: lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,6 @@ export default defineConfig([
     safeAlign, {
         ignores: ['example.*', '**/fixture'],
         rules: {
-            "n/no-unpublished-import",
             '@typescript-eslint/ban-ts-comment': 'off',
             '@typescript-eslint/no-unused-vars': 'off',
             '@typescript-eslint/no-unused-expressions': 'off',
